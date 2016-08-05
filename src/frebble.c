5f9ef0229b784689b54ef51a15bcf6c49f423696
Stephan Pavlovic <stephan@railslove.com>
2016-08-05 17:02:05 +0200

    maybe this helps to run the js

--- src/frebble.c
+++ src/frebble.c
@@ -3,58 +3,82 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static char s_text_buffer[64];
+
+enum {
+  APP_KEY_READY,
+  APP_KEY_TEXT,
+  APP_KEY_ACTION,
+  APP_KEY_QUIT,
+};
+
+static void in_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *tuple;
+
+  // send the launch_arg when we receive ready from JS
+  tuple = dict_find(iter, APP_KEY_READY);
+  if (tuple) {
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    dict_write_uint32(iter, APP_KEY_ACTION, launch_get_args());
+    dict_write_end(iter);
+    app_message_outbox_send();
+  }
+
+  // update s_text_buffer
+  tuple = dict_find(iter, APP_KEY_TEXT);
+  if (tuple) {
+    snprintf(s_text_buffer, sizeof(s_text_buffer), "%s", tuple->value->cstring);
+    text_layer_set_text(s_text_layer, s_text_buffer);
+  }
+
+  // quit the app
+  tuple = dict_find(iter, APP_KEY_QUIT);
+  if (tuple) {
+    window_stack_pop_all(false);
+  }
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
+static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
+  s_text_layer = text_layer_create(grect_inset(bounds, GEdgeInsets(30, 3)));
+  text_layer_set_text(s_text_layer, "Loading...");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(s_text_layer, GColorClear);
+  text_layer_set_text_color(s_text_layer, GColorCeleste);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+
+#if defined(PBL_ROUND)
+  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 5);
+#endif
 }
 
-static void prv_window_unload(Window *window) {
+static void window_unload(Window *window) {
   text_layer_destroy(s_text_layer);
 }
 
-static void prv_init(void) {
+static void init(void) {
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
+  window_set_background_color(s_window, GColorCobaltBlue);
   window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
+    .load = window_load,
+    .unload = window_unload,
   });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+  window_stack_push(s_window, false);
+
+  app_message_register_inbox_received(in_received_handler);
+  app_message_open(256, 256);
 }
 
-static void prv_deinit(void) {
+static void deinit(void) {
   window_destroy(s_window);
 }
 
 int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
+  init();
   app_event_loop();
-  prv_deinit();
+  deinit();
 }
